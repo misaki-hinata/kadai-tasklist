8863119f2e20847d89fb64e346c0c5b4033e0177
hinata <hinata@NOTE1016>
2021-07-27 11:31:43 +0900

    getAllMessagesを追加(persistence.xmlのJDBC接続プロパティーのURLが間違っていたので変更)

--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -2,6 +2,8 @@ package models;
 
 import java.sql.Timestamp;
 
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,12 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "tasks")
 public class Task {
     @Id
